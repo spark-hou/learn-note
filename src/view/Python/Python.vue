5489165f6d9111afbbfe8aeb947d4e76d9e94f59
sparkHou <1>
2021-06-08 00:16:16 +0800

    脑图demo

--- src/view/Python/Python.vue
+++ src/view/Python/Python.vue
@@ -9,6 +9,13 @@
       />
     </a-anchor>
     <div class="g-container">
+      <a-page-header
+        style="border: 1px solid rgb(235, 237, 240)"
+        :title="title"
+        @back="backUp"
+      />
+      <div class="g-block"></div>
+      <div class="g-block"></div>
       <div id="g6"></div>
       <div style="height: 4000px">
 
@@ -26,11 +33,18 @@
 
 <script lang="ts">
 import {defineComponent, onMounted} from 'vue';
+import {useRouter} from 'vue-router'
 import G6 from '@antv/g6';
 
 export default defineComponent({
   name: 'Python',
   setup() {
+    let title = 'Python笔记'
+    let router = useRouter()
+    let backUp = () => {
+      router.back()
+    }
+    //知识图谱树
     onMounted(() => {
       const container: any = document.getElementById('g6');
       const width = container.scrollWidth;
@@ -87,12 +101,12 @@ export default defineComponent({
       let data = {
         id: "Modeling Methods",
         children: [
-          {id: 'test'},
           {id: 'test1'},
           {id: 'test2'},
           {id: 'test3'},
         ]
       }
+
       graph.node(function (node) {
         console.log(node, '====')
         if (node.id === 'Modeling Methods') {
@@ -119,91 +133,10 @@ export default defineComponent({
       //     graph.changeSize(container.scrollWidth, container.scrollHeight);
       //   };
     })
-    // let setChart = () => {
-    //   const container = document.getElementById('container');
-    //   const width = container.scrollWidth;
-    //   const height = container.scrollHeight || 500;
-    //   const graph = new G6.TreeGraph({
-    //     container: 'container',
-    //     width,
-    //     height,
-    //     modes: {
-    //       default: [
-    //         {
-    //           type: 'collapse-expand',
-    //           onChange: function onChange(item, collapsed) {
-    //             const data = item.get('model');
-    //             data.collapsed = collapsed;
-    //             return true;
-    //           },
-    //         },
-    //         'drag-canvas',
-    //         'zoom-canvas',
-    //       ],
-    //     },
-    //     defaultNode: {
-    //       size: 26,
-    //       anchorPoints: [
-    //         [0, 0.5],
-    //         [1, 0.5],
-    //       ],
-    //     },
-    //     defaultEdge: {
-    //       type: 'cubic-horizontal',
-    //     },
-    //     layout: {
-    //       type: 'mindmap',
-    //       direction: 'H',
-    //       getHeight: () => {
-    //         return 16;
-    //       },
-    //       getWidth: () => {
-    //         return 16;
-    //       },
-    //       getVGap: () => {
-    //         return 10;
-    //       },
-    //       getHGap: () => {
-    //         return 100;
-    //       },
-    //       getSide: () => {
-    //         return 'right';
-    //       },
-    //     },
-    //   });
-    //
-    //   let centerX = 0;
-    //   graph.node(function (node) {
-    //     if (node.id === 'Modeling Methods') {
-    //       centerX = node.x;
-    //     }
-    //
-    //     return {
-    //       label: node.id,
-    //       labelCfg: {
-    //         position:
-    //             node.children && node.children.length > 0
-    //               ? 'right'
-    //               : node.x > centerX
-    //                 ? 'right'
-    //                 : 'left',
-    //         offset: 5,
-    //       },
-    //     };
-    //   });
-    //
-    //   graph.data(data);
-    //   graph.render();
-    //   graph.fitView();
-    //
-    //   if (typeof window !== 'undefined')
-    //     window.onresize = () => {
-    //       if (!graph || graph.get('destroyed')) return;
-    //       if (!container || !container.scrollWidth || !container.scrollHeight) return;
-    //       graph.changeSize(container.scrollWidth, container.scrollHeight);
-    //     };
-    // }
-    return {}
+    return {
+      title,
+      backUp
+    }
   }
 });
 </script>
@@ -219,8 +152,10 @@ export default defineComponent({
 <style lang="less">
   .Python {
     #g6 {
-      width: 400px;
-      height: 400px;
+      max-width: 1200px;
+      min-width: 800px;
+      height: 800px;
+      border: 1px solid black;
     }
   }
 
