d407ed101a5790cce158bef99a35af80fcbcda03
sparkHou <1>
2021-06-16 00:18:24 +0800

    完善脑图

--- src/view/Python/Python.vue
+++ src/view/Python/Python.vue
@@ -4,12 +4,17 @@
       class="toc-affix"
     >
       <a-anchor-link
+        href="#Top"
+        title="Top"
+      />
+      <a-anchor-link
         href="#haha"
         title="Basic demo"
       />
     </a-anchor>
     <div class="g-container">
       <a-page-header
+        id="Top"
         style="border: 1px solid rgb(235, 237, 240)"
         :title="title"
         @back="backUp"
@@ -36,6 +41,314 @@ import {defineComponent, onMounted} from 'vue';
 import {useRouter} from 'vue-router'
 import G6 from '@antv/g6';
 
+const {Util} = G6;
+const colorArr = [
+  '#5B8FF9',
+  '#5AD8A6',
+  '#5D7092',
+  '#F6BD16',
+  '#6F5EF9',
+  '#6DC8EC',
+  '#D3EEF9',
+  '#DECFEA',
+  '#FFE0C7',
+  '#1E9493',
+  '#BBDEDE',
+  '#FF99C3',
+  '#FFE0ED',
+  '#CDDDFD',
+  '#CDF3E4',
+  '#CED4DE',
+  '#FCEBB9',
+  '#D3CEFD',
+  '#945FB9',
+  '#FF9845',
+];
+G6.registerNode(
+  'dice-mind-map-root', {
+    jsx: (cfg: any) => {
+      const width = Util.getTextSize(cfg.label, 16)[0] + 24;
+      const stroke = cfg.style.stroke || '#096dd9';
+      // const fill = cfg.style.fill;
+
+      return `
+      <group>
+        <rect draggable="true" style={{width: ${width}, height: 42, stroke: ${stroke}, radius: 4}} keyshape>
+          <text style={{ fontSize: 16, marginLeft: 12, marginTop: 12 }}>${cfg.label}</text>
+          <text style={{ marginLeft: ${
+  width - 16
+}, marginTop: -20, stroke: '#66ccff', fill: '#000', cursor: 'pointer', opacity: ${
+  cfg.hover ? 0.75 : 0
+} }} action="add">+</text>
+        </rect>
+      </group>
+    `;
+    },
+    getAnchorPoints() {
+      return [
+        [0, 0.5],
+        [1, 0.5],
+      ];
+    },
+  },
+  'single-node',
+);
+G6.registerNode(
+  'dice-mind-map-sub', {
+    jsx: (cfg: any) => {
+      const width = Util.getTextSize(cfg.label, 14)[0] + 24;
+      const color = cfg.color || cfg.style.stroke;
+
+      return `
+      <group>
+        <rect draggable="true" style={{width: ${width + 24}, height: 22}} keyshape>
+          <text draggable="true" style={{ fontSize: 14, marginLeft: 12, marginTop: 6 }}>${
+  cfg.label
+}</text>
+          <text style={{ marginLeft: ${
+  width - 8
+}, marginTop: -10, stroke: ${color}, fill: '#000', cursor: 'pointer', opacity: ${
+  cfg.hover ? 0.75 : 0
+}, next: 'inline' }} action="add">+</text>
+          <text style={{ marginLeft: ${
+  width - 4
+}, marginTop: -10, stroke: ${color}, fill: '#000', cursor: 'pointer', opacity: ${
+  cfg.hover ? 0.75 : 0
+}, next: 'inline' }} action="delete">-</text>
+        </rect>
+        <rect style={{ fill: ${color}, width: ${width + 24}, height: 2, x: 0, y: 22 }} />
+
+      </group>
+    `;
+    },
+    getAnchorPoints() {
+      return [
+        [0, 0.965],
+        [1, 0.965],
+      ];
+    },
+  },
+  'single-node',
+);
+G6.registerNode(
+  'dice-mind-map-leaf', {
+    jsx: (cfg: any) => {
+      const width = Util.getTextSize(cfg.label, 12)[0] + 24;
+      const color = cfg.color || cfg.style.stroke;
+
+      return `
+      <group>
+        <rect draggable="true" style={{width: ${width + 20}, height: 26, fill: 'transparent' }}>
+          <text style={{ fontSize: 12, marginLeft: 12, marginTop: 6 }}>${cfg.label}</text>
+              <text style={{ marginLeft: ${
+  width - 8
+}, marginTop: -10, stroke: ${color}, fill: '#000', cursor: 'pointer', opacity: ${
+  cfg.hover ? 0.75 : 0
+}, next: 'inline' }} action="add">+</text>
+              <text style={{ marginLeft: ${
+  width - 4
+}, marginTop: -10, stroke: ${color}, fill: '#000', cursor: 'pointer', opacity: ${
+  cfg.hover ? 0.75 : 0
+}, next: 'inline' }} action="delete">-</text>
+        </rect>
+        <rect style={{ fill: ${color}, width: ${width + 24}, height: 2, x: 0, y: 32 }} />
+
+      </group>
+    `;
+    },
+    getAnchorPoints() {
+      return [
+        [0, 0.965],
+        [1, 0.965],
+      ];
+    },
+  },
+  'single-node',
+);
+G6.registerBehavior('dice-mindmap', {
+  getEvents() {
+    return {
+      'node:click': 'clickNode',
+      'node:dblclick': 'editNode',
+      'node:mouseenter': 'hoverNode',
+      'node:mouseleave': 'hoverNodeOut',
+    };
+  },
+  clickNode(evt: any) {
+    const model = evt.item.get('model');
+    const name = evt.target.get('action');
+    switch (name) {
+    case 'add':
+      // eslint-disable-next-line no-case-declarations
+      const newId =
+            model.id +
+            '-' +
+            (((model.children || []).reduce((a: any, b: any) => {
+              const num = Number(b.id.split('-').pop());
+              return a < num ? num : a;
+            }, 0) || 0) +
+              1);
+      evt.currentTarget.updateItem(evt.item, {
+        children: (model.children || []).concat([{
+          id: newId,
+          direction: newId.charCodeAt(newId.length - 1) % 2 === 0 ? 'right' : 'left',
+          label: 'New',
+          type: 'dice-mind-map-leaf',
+          color: model.color || colorArr[Math.floor(Math.random() * colorArr.length)],
+        },]),
+      });
+      evt.currentTarget.layout(false);
+      break;
+    case 'delete':
+      // eslint-disable-next-line no-case-declarations
+      const parent = evt.item.get('parent');
+      evt.currentTarget.updateItem(parent, {
+        children: (parent.get('model').children || []).filter((e: any) => e.id !== model.id),
+      });
+      evt.currentTarget.layout(false);
+      break;
+    case 'edit':
+      break;
+    default:
+      return;
+    }
+  },
+  editNode(evt: any) {
+    const item = evt.item;
+    const model = item.get('model');
+    const {
+      x,
+      y
+    } = item.calculateBBox();
+    const graph = evt.currentTarget;
+    const realPosition = evt.currentTarget.getClientByPoint(x, y);
+    const el = document.createElement('div');
+    const fontSizeMap: any = {
+      'dice-mind-map-root': 24,
+      'dice-mind-map-sub': 18,
+      'dice-mind-map-leaf': 16,
+    };
+    el.style.fontSize = fontSizeMap[model.type] + 'px';
+    el.style.position = 'fixed';
+    el.style.top = realPosition.y + 'px';
+    el.style.left = realPosition.x + 'px';
+    el.style.paddingLeft = '12px';
+    el.style.transformOrigin = 'top left';
+    el.style.transform = `scale(${evt.currentTarget.getZoom()})`;
+    const input = document.createElement('input');
+    input.style.border = 'none';
+    input.value = model.label;
+    input.style.width = Util.getTextSize(model.label, fontSizeMap[model.type])[0] + 'px';
+    input.className = 'dice-input';
+    el.className = 'dice-input';
+    el.appendChild(input);
+    document.body.appendChild(el);
+    const destroyEl = () => {
+      document.body.removeChild(el);
+    };
+    const clickEvt = (event: any) => {
+      if (!(event.target && event.target.className && event.target.className.includes('dice-input'))) {
+        window.removeEventListener('mousedown', clickEvt);
+        window.removeEventListener('scroll', clickEvt);
+        graph.updateItem(item, {
+          label: input.value,
+        });
+        graph.layout(false);
+        graph.off('wheelZoom', clickEvt);
+        destroyEl();
+      }
+    };
+    graph.on('wheelZoom', clickEvt);
+    window.addEventListener('mousedown', clickEvt);
+    window.addEventListener('scroll', clickEvt);
+    input.addEventListener('keyup', (event) => {
+      if (event.key === 'Enter') {
+        clickEvt({
+          target: {},
+        });
+      }
+    });
+  },
+  hoverNode(evt: any) {
+    evt.currentTarget.updateItem(evt.item, {
+      hover: true,
+    });
+  },
+  hoverNodeOut(evt: any) {
+    evt.currentTarget.updateItem(evt.item, {
+      hover: false,
+    });
+  },
+});
+G6.registerBehavior('scroll-canvas', {
+  getEvents: function getEvents() {
+    return {
+      wheel: 'onWheel',
+    };
+  },
+
+  onWheel: function onWheel(ev: any) {
+    const graph: any = this.graph;
+    if (!graph) {
+      return;
+    }
+    if (ev.ctrlKey) {
+      const canvas = graph.get('canvas');
+      const point = canvas.getPointByClient(ev.clientX, ev.clientY);
+      let ratio = graph.getZoom();
+      if (ev.wheelDelta > 0) {
+        ratio += ratio * 0.05;
+      } else {
+        ratio *= ratio * 0.05;
+      }
+      graph.zoomTo(ratio, {
+        x: point.x,
+        y: point.y,
+      });
+    } else {
+      const x = ev.deltaX || ev.movementX;
+      const y = ev.deltaY || ev.movementY || (-ev.wheelDelta * 125) / 3;
+      graph.translate(-x, -y);
+    }
+    ev.preventDefault();
+  },
+});
+const dataTransform = (data:any) => {
+  const changeData = (d:any, level = 0, color:any='') => {
+    const data = {
+      ...d,
+    };
+    switch (level) {
+    case 0:
+      data.type = 'dice-mind-map-root';
+      break;
+    case 1:
+      data.type = 'dice-mind-map-sub';
+      break;
+    default:
+      data.type = 'dice-mind-map-leaf';
+      break;
+    }
+
+    data.hover = false;
+
+    if (color) {
+      data.color = color;
+    }
+
+    if (level === 1 && !d.direction) {
+      if (!d.direction) {
+        data.direction = d.id.charCodeAt(d.id.length - 1) % 2 === 0 ? 'right' : 'left';
+      }
+    }
+
+    if (d.children) {
+      data.children = d.children.map((child:any) => changeData(child, level + 1, data.color));
+    }
+    return data;
+  };
+  return changeData(data);
+};
 export default defineComponent({
   name: 'Python',
   setup() {
@@ -45,93 +358,184 @@ export default defineComponent({
       router.back()
     }
     //知识图谱树
-    onMounted(() => {
+    // onMounted(() => {
+    //   const container: any = document.getElementById('g6');
+    //   const width = container.scrollWidth;
+    //   const height = container.scrollHeight || 500;
+    //   const graph = new G6.TreeGraph({
+    //     container: 'g6',
+    //     width,
+    //     height,
+    //     modes: {
+    //       default: [
+    //         {
+    //           type: 'collapse-expand',
+    //           onChange: function onChange(item: any, collapsed) {
+    //             const data = item.get('model');
+    //             data.collapsed = collapsed;
+    //             return true;
+    //           },
+    //         },
+    //         'drag-canvas',
+    //         'zoom-canvas',
+    //       ],
+    //     },
+    //     defaultNode: {
+    //       size: 26,
+    //       anchorPoints: [
+    //         [0, 0.5],
+    //         [1, 0.5],
+    //       ],
+    //     },
+    //     defaultEdge: {
+    //       type: 'cubic-horizontal',
+    //     },
+    //     layout: {
+    //       type: 'mindmap',
+    //       direction: 'H',
+    //       getHeight: () => {
+    //         return 16;
+    //       },
+    //       getWidth: () => {
+    //         return 16;
+    //       },
+    //       getVGap: () => {
+    //         return 10;
+    //       },
+    //       getHGap: () => {
+    //         return 100;
+    //       },
+    //       getSide: () => {
+    //         return 'right';
+    //       },
+    //     },
+    //   });
+    //   let centerX: any = 0;
+    //   let data = {
+    //     id: "Modeling Methods",
+    //     children: [
+    //       {id: 'test1'},
+    //       {id: 'test2'},
+    //       {id: 'test3'},
+    //     ]
+    //   }
+    //
+    //   graph.node(function (node: any) {
+    //     console.log(node, '====')
+    //     if (node.id === 'Modeling Methods') {
+    //       centerX = node.x;
+    //     }
+    //     return {
+    //       label: node.id,
+    //       labelCfg: {
+    //         position:
+    //         // @ts-ignore
+    //             node.children && node.children.length > 0 ? 'right' : node.x > centerX ? 'right' : 'left', offset: 5,
+    //       },
+    //     };
+    //   });
+    //
+    //   graph.data(data);
+    //   graph.render();
+    //   graph.fitView();
+    //
+    //   // if (typeof window !== 'undefined')
+    //   //   window.onresize = () => {
+    //   //     if (!graph || graph.get('destroyed')) return;
+    //   //     if (!container || !container.scrollWidth || !container.scrollHeight) return;
+    //   //     graph.changeSize(container.scrollWidth, container.scrollHeight);
+    //   //   };
+    // })
+    const rawData = {
+      label: 'Modeling Methods',
+      id: '0',
+      children: [{
+        label: 'Classification',
+        id: '0-1',
+        children: [{
+          label: 'Logistic regression',
+          id: '0-1-1',
+        },
+        {
+          label: 'Linear discriminant analysis',
+          id: '0-1-2',
+        },
+        {
+          label: 'Rules',
+          id: '0-1-3',
+        },
+        {
+          label: 'Decision trees',
+          id: '0-1-4',
+        },
+        {
+          label: 'Naive Bayes',
+          id: '0-1-5',
+        },
+        {
+          label: `Support vectomachine`,
+          id: '0-1-6',
+        },
+        {
+          label: 'Probabilistic neural network',
+          id: '0-1-7',
+        },
+        {
+          label: `Support vectomachine`,
+          id: '0-1-8',
+        },
+        ],
+      },
+      ],
+    };
+    const renderTree = () => {
       const container: any = document.getElementById('g6');
+
       const width = container.scrollWidth;
-      const height = container.scrollHeight || 500;
-      const graph = new G6.TreeGraph({
-        container: 'g6',
+      const height = (container.scrollHeight || 500) - 20;
+      const tree = new G6.TreeGraph({
+        container: container,
         width,
         height,
-        modes: {
-          default: [
-            {
-              type: 'collapse-expand',
-              onChange: function onChange(item: any, collapsed) {
-                const data = item.get('model');
-                data.collapsed = collapsed;
-                return true;
-              },
-            },
-            'drag-canvas',
-            'zoom-canvas',
-          ],
-        },
-        defaultNode: {
-          size: 26,
-          anchorPoints: [
-            [0, 0.5],
-            [1, 0.5],
-          ],
-        },
-        defaultEdge: {
-          type: 'cubic-horizontal',
-        },
+        fitView: true,
+        fitViewPadding: [10, 20],
         layout: {
           type: 'mindmap',
           direction: 'H',
           getHeight: () => {
             return 16;
           },
-          getWidth: () => {
-            return 16;
+          getWidth: (node: any) => {
+            return node.level === 0 ?
+              Util.getTextSize(node.label, 16)[0] + 12 :
+              Util.getTextSize(node.label, 12)[0];
           },
           getVGap: () => {
             return 10;
           },
           getHGap: () => {
-            return 100;
+            return 60;
           },
           getSide: () => {
             return 'right';
           },
         },
-      });
-      let centerX: any = 0;
-      let data = {
-        id: "Modeling Methods",
-        children: [
-          {id: 'test1'},
-          {id: 'test2'},
-          {id: 'test3'},
-        ]
-      }
-
-      graph.node(function (node:any) {
-        console.log(node, '====')
-        if (node.id === 'Modeling Methods') {
-          centerX = node.x;
-        }
-        return {
-          label: node.id,
-          labelCfg: {
-            position:
-            // @ts-ignore
-                node.children && node.children.length > 0 ? 'right' : node.x > centerX ? 'right' : 'left', offset: 5,
+        defaultEdge: {
+          type: 'cubic-horizontal',
+          style: {
+            lineWidth: 2,
           },
-        };
+        },
+        minZoom: 0.5,
+        modes: {
+          default: ['drag-canvas', 'zoom-canvas', 'dice-mindmap'],
+        },
       });
-
-      graph.data(data);
-      graph.render();
-      graph.fitView();
-
-      // if (typeof window !== 'undefined')
-      //   window.onresize = () => {
-      //     if (!graph || graph.get('destroyed')) return;
-      //     if (!container || !container.scrollWidth || !container.scrollHeight) return;
-      //     graph.changeSize(container.scrollWidth, container.scrollHeight);
-      //   };
+      tree.data(dataTransform(rawData));
+      tree.render();
+    }
+    onMounted(() => {
+      renderTree()
     })
     return {
       title,
