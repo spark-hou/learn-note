e84a59d68406fa7e8be60839d4436e8bd2ceca6c
侯大大 <1>
2021-06-23 23:29:50 +0800

    11

--- src/plugin/antd/index.ts
+++ src/plugin/antd/index.ts
@@ -4,10 +4,15 @@
  * @Description:
  */
 import {Button, Switch, Anchor,PageHeader,Typography } from 'ant-design-vue/es';
-const{title}=Typography
+const{title,paragraph,text,link  }=Typography
 export default function (app: any) {
   app.use(Button)
   app.use(Switch)
   app.use(Anchor)
   app.use(PageHeader)
+  app.use(Typography)
+  app.use(title)
+  app.use(paragraph)
+  app.use(text)
+  app.use(link)
 }
